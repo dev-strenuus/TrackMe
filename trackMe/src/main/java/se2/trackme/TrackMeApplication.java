d7d958f01f3c8f92af4b6c49f909c86a64229434
romeopaolo <paolo1.romeo@mail.polimi.it>
2018-12-14 18:53:30 +0100

    implementation configuration setup

---
+++ trackMe/src/main/java/se2/trackme/TrackMeApplication.java
@@ -0,0 +1,14 @@
+package se2.trackme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrackMeApplication.class, args);
+    }
+
+}
+
